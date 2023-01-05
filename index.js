22ac0e2b8bb5cf4baea9af12de07cdac86de8f5f
Stephen Molloy <molloyitsolutions@yahoo.co.uk>
2023-01-05 21:31:06 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-setInterval(cb, 30000);
-
 let cb = () => {
     console.log("Interval");
 }
+
+setInterval(cb, 30000);
 
 console.log("Hello twitter!");
